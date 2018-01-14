4bb6e400385f1423999bdbcd99f974f1b58f345a
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-14 06:57:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ function add(num1, num2){
   return num1 + num2;
 }
 
-add(14, 11)
+
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
 
