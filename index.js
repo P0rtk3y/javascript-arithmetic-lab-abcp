e225cfd21a7add56d89fdc5a38ee15610ca4a800
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-14 06:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function multiply(num1, num2){
   return num1 * num2;
 }
 
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+function inc(num1){
+  return num1++;
+}
+
+function dec(num1){
+  return num1--
+}
